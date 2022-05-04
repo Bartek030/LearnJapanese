6508d8312c5c2c3a37e55a6b551b87ca9dcc66e8
Bartlomiej Swies <bartlomiejswies@gmail.com>
2022-05-04 21:01:55 +0200

    Initial commit

---
+++ src/test/java/pl/bartlomiej_swies/LearnJapanese/LearnJapaneseApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bartlomiej_swies.LearnJapanese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJapaneseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
