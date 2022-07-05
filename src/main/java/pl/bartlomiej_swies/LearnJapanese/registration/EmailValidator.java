26d4d4a723a241a7b5c1196151e34d2756c831fa
Bartlomiej Swies <bartlomiejswies@gmail.com>
2022-07-05 23:54:58 +0200

    Email validation - regex added

--- src/main/java/pl/bartlomiej_swies/LearnJapanese/registration/EmailValidator.java
+++ src/main/java/pl/bartlomiej_swies/LearnJapanese/registration/EmailValidator.java
@@ -3,13 +3,20 @@ package pl.bartlomiej_swies.LearnJapanese.registration;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 class EmailValidator implements Predicate<String> {
 
+    private final static String EMAIL_REGEX_PATTERN = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+
     @Override
     public boolean test(final String s) {
-        // TODO: Regex to validate email
-        return true;
+
+        Pattern pattern = Pattern.compile(EMAIL_REGEX_PATTERN);
+        Matcher matcher = pattern.matcher(s);
+
+        return matcher.find();
     }
 }
