00492c5cc0d4205d3a49f6a1c8059e5e1014a3be
Bartlomiej Swies <bartlomiejswies@gmail.com>
2022-07-06 00:00:18 +0200

    Email validation - regex added

--- src/main/java/pl/bartlomiej_swies/LearnJapanese/registration/EmailValidator.java
+++ src/main/java/pl/bartlomiej_swies/LearnJapanese/registration/EmailValidator.java
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
 @Service
 class EmailValidator implements Predicate<String> {
 
-    private final static String EMAIL_REGEX_PATTERN = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+    private final static String EMAIL_REGEX_PATTERN = "^(.+)@(\\S+)$";
 
     @Override
     public boolean test(final String s) {
@@ -17,6 +17,6 @@ class EmailValidator implements Predicate<String> {
         Pattern pattern = Pattern.compile(EMAIL_REGEX_PATTERN);
         Matcher matcher = pattern.matcher(s);
 
-        return matcher.find();
+        return matcher.matches();
     }
 }
